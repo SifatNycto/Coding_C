420c9ff929a91365729b516bd7c28476bb47da99
SifatNycto <sftmhd@gmail.com>
2026-07-13 20:19:39 +0600

    after a long time

---
+++ Random_C/rand.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("enter a number: ");
+    scanf("%d%d", &a, &b);
+    
+    printf("\nsum of a and b is: %d", a+b);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main() {
+
+// char word1[600]= "";
+// char word2[600]= "";
+
+// printf("Enter a word");
+// scanf("%s", word1);
+
+// int length = strlen(word1);
+
+//     if(islower(word1[0])) {
+        
+//       for(int i = 0; i < length; i++){
+//             word2[i] = tolower(word1[i]);
+//         }
+//     }
+//     else
+//     for (int i = 0; i < length; i++){
+//         word2[i] = toupper(word1[i]);
+
+//     }
+//      printf ("%s",word2);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Random_C/random.c
+++ Random_C/random.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
 
 // int main()
 // {
@@ -92,14 +96,232 @@
 
 
 
+// int main()
+// {
+//   int n;
+
+//   while(n % 2 != 0)
+//   {
+//     scanf("%d", &n);
+//   }
+
+//   return 0;
+// }
+
+
+
+// int main()
+// {
+//   int n;
+
+//   for(;;)
+//   {
+//     scanf("%d", &n);
+//     if (n % 2 == 0)
+//       break;
+//   }
+
+//   return 0;
+// }
+
+
+
+
+
+// #include <stdio.h>
+
+// int main()
+// {
+//   int num;
+
+//   printf("Enter an even number: ");
+//   scanf("%d", &num);
+
+//   while (num % 2 != 0)
+//   {
+//     printf("Wrong input! \nEnter an even number: ");
+//     scanf("%d", &num);
+//   }
+
+//   printf("Correct! You entered an even number.");
+
+//   return 0;
+// }
+
+
+
+// #include <string.h>
+// int main()
+// {
+//   int n;
+
+//   printf("\nhow many names you wanna enter: ");
+//   scanf("%i", &n);
+
+
+//   char s[n][100];
+
+//   printf("enter your inputs: ");
+//   for (int i = 0; i < n; i++)
+//   {
+//     scanf(" %s", s[i]);
+//   }
+
+//   printf("\nyour entered inputs");
+//   for(int i = 0; i < n; i++)
+//   {
+//     printf("\n%s", s[i]);
+    
+//   }
+
+//   return 0;
+// }
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//   int n;
+//   scanf("%d", &n);
+
+//   char word[n][100];
+
+//   for(int i = 0; i < n; i++)
+//   {
+//     scanf(" %s", word[i]);
+//     int length = strlen(word[i]);
+
+//     char first_char = word[i][0];
+//     char last_char = word[i][length - 1];
+
+//     if (length > 10)
+//     {
+//       printf("%c%d%c", first_char, (length - 2), last_char);
+//     }
+
+//     else 
+//     {
+//       printf("%s", word[i]);
+//     }
+
+//   }
+
+//   return 0;
+// }
+
+
+
+
+
+// #include <string.h>
+
+// int main()
+// {
+//   int n;
+//   scanf("%d", &n);
+
+//   char word[n][100];
+
+//   for(int i = 0; i < n; i++)
+//   {
+//     scanf(" %s", word[i]);
+//   }
+
+//   for (int i = 0; i < n; i++)
+//   {
+//     int length = strlen(word[i]);
+
+//     char first_char = word[i][0];
+//     char last_char = word[i][length - 1];
+
+//     if (length > 10)
+//     {
+//       printf("\n%c%d%c", first_char, (length - 2), last_char);
+//     }
+
+//     else 
+//     {
+//       printf("\n%s", word[i]);
+//     }
+//   }
+
+
+//   return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main()
+// {
+  
+//   char s1[69] = "";
+//   char s2[65] = "";
+
+//   printf("\nenter a string: ");
+//   scanf(" %s", s1);
+
+//   int length = strlen(s1);
+
+//   for (int i = 0; i < length; i++)
+//   {
+//     s2[i] = tolower(s1[i]);
+//   }
+
+//   // printf("%s", s2);
+
+//   for (int i = 0; i < length; i++)
+//   {
+//     printf("%c", s2[i]);
+//   }
+
+//   return 0;
+// }
+
+
+
+
+
+
+
 int main()
 {
-    int n;
+  char word1[500] = "";
+  char word2[500] = "";
 
-    while(n % 2 != 0)
+  printf("\nEnter any word: ");
+  scanf(" %s", word1);
+
+  int length = strlen(word1);
+
+  if (islower(word1[0]))
+  {
+    for (int i = 0; i < length; i++)
     {
-        scanf("%d", &n);
+      word2[i] = tolower(word1[i]);
     }
+  }
+  else
+  {
+    for (int i = 0; i < length; i++)
+    {
+      word2[i] = toupper(word1[i]);
+    }
+  }
 
-    return 0;
+  printf("%s", word2);
+
+  return 0;
 }
